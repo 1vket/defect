385dc38e3df3c9453817757267763d4520750bcf
1vket <65165408+1vket@users.noreply.github.com>
2022-04-04 13:08:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ install_requires = [
 ]
 
 packages = [
-  'nwskwii',
+  'defect',
 ]
 
 setup(
-  name='nwskwii',
-  version='0.1',
+  name='defect',
+  version='1.1',
   packages=packages,
   install_requires=install_requires,
 )
